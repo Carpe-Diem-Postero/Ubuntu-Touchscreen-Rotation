2547a1484bda244a56eb4597c63a9888f3184b00
Carpe Diem <traumaguy@protonmail.com>
2022-04-26 17:05:10 +0200

    Minor changes

--- Landscape.sh
+++ Landscape.sh
@@ -1,3 +1,3 @@
-screen="" #Set the name of your screen here. You can check it with "xinput list" in terminal
+screen="FTSC1015:00 2808:1015" #Set the name of your screen here. You can check it with "xinput list" in terminal
 xrandr -o right
 xinput set-prop "$screen" 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
--- Screen-Tearing.sh
+++ Screen-Tearing.sh
@@ -1,6 +1,6 @@
-sudo mkdir -p /etc/X11/xorg.conf.d
-sudo touch /etc/X11/xorg.conf.d/20-intel.conf
-sudo echo 'Section "Device"
+mkdir -p /etc/X11/xorg.conf.d
+touch /etc/X11/xorg.conf.d/20-intel.conf
+echo 'Section "Device"
     Identifier "Intel Graphics"
     Driver "intel"
     Option "TearFree"    "true"
